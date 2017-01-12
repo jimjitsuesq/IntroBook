28bfb6b6fc3a969851e42a28c06eb00f71b73ce2
Jim Peters <jimjitsuesq@gmail.com>
2017-01-12 09:31:36 -0800

    All programs written in exercises for 'The Basics' Chapter

---
+++ TheBasics3.rb
@@ -0,0 +1,7 @@
+movie_hash={1975 => '1975 Movie', 2004 => '2004 Movie', 2013 => '2013 Movie', 2001 => '2001 Movie', 1981 => '1981 Movie'}
+
+puts movie_hash[1975]
+puts movie_hash[2004]
+puts movie_hash[2013]
+puts movie_hash[2001]
+puts movie_hash[1981]
