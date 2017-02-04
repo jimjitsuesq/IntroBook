d9ecf4dc5d2266b6ce38cb1ece10c0d54f063932
Jim Peters <jimjitsuesq@gmail.com>
2017-02-04 07:47:15 -0800

    Array and Hash Exercises

---
+++ Arrays7.rb
@@ -0,0 +1,6 @@
+arr = [1, 2, 3, 4]
+
+add2=arr.map {|add| add+1}
+
+p arr
+p add2
---
+++ hashes2.rb
@@ -0,0 +1,11 @@
+hasha = {color: 'red', shape: 'round', number: '1'}
+
+hashb = {team: 'c', age: '5'}
+
+      newhash1 = hasha.merge(hashb)
+
+      newhash2 = hasha.merge!(hashb)  
+
+        puts hasha
+
+        puts hashb
